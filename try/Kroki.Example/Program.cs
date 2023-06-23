933bd20243b20c9a2acdb91ae075cd13c8c40441
xafero <georg.berendt@outlook.com>
2023-06-23 22:49:49 +0200

    Adding core structure

---
+++ try/Kroki.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kroki.Example
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
