71486d3cf0a53ef22abd61555189e91740bf3f03
xafero <georg.berendt@outlook.com>
2023-07-03 21:14:55 +0200

    Adding core of demo site

---
+++ try/Kroki.Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.FileSystemGlobbing;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kroki.Demo
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(_ => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
