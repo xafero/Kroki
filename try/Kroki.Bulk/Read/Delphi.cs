a5ca8b282d3de58eda566a56d25c44e70087788c
xafero <georg.berendt@outlook.com>
2023-07-04 17:55:30 +0200

    Now recreating project structure with solution files

--- try/Kroki.Bulk/Read/Delphi.cs
+++ try/Kroki.Bulk/Read/Delphi.cs
@@ -3,71 +3,78 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Kroki.MSBuild;
 using static Kroki.Core.Util.Helpers;
 
 namespace Kroki.Bulk.Read
 {
-    internal static class Delphi
-    {
-	    public static void ParsePas(string file, string outFile)
-        {
-            outFile = outFile.Replace(".pas", ".cs");
-            ParsePascal(file, outFile);
-        }
+	internal static class Delphi
+	{
+		internal static Action<string>? Log;
 
-        private static void ParsePascal(string file, string outFile)
-        {
-            var pasLines = new List<string>(File.ReadAllLines(file, Utf));
+		public static void ParsePas(string file, string outFile)
+		{
+			outFile = outFile.Replace(".pas", ".cs");
+			ParsePascal(file, outFile);
+		}
 
-            using var stream = File.Create(outFile);
-            using var code = new StreamWriter(stream, Utf);
+		private static void ParsePascal(string file, string outFile)
+		{
+			var pasLines = new List<string>(File.ReadAllLines(file, Utf));
 
-            TransformComment(pasLines, code);
+			using var stream = File.Create(outFile);
+			using var code = new StreamWriter(stream, Utf);
 
-            var src = ReadSource(file);
+			TransformComment(pasLines, code);
 
-            var (_, csCode) = Translate(file, src);
-            code.Write(csCode);
+			var src = ReadSource(file);
 
-            Console.WriteLine($"   * Converted to '{outFile}'...");
-        }
+			var (_, csCode) = Translate(file, src);
+			code.Write(csCode);
+
+			Log!.Invoke($"   * Converted to '{outFile}'...");
+		}
 
 		public static void ParseDfm(string file, string outFile)
-        {
-	        outFile = outFile.Replace(".dfm", ".d.cs");
-	        ParsePascal(file, outFile);
-        }
+		{
+			outFile = outFile.Replace(".dfm", ".d.cs");
+			ParsePascal(file, outFile);
+		}
 
-        public static void ParseDpr(string file, string outFile)
-        {
-	        outFile = outFile.Replace(".dpr", ".p.cs");
-	        ParsePascal(file, outFile);
-        }
+		public static void ParseDpr(string file, string outFile)
+		{
+			outFile = outFile.Replace(".dpr", ".cs");
+			ParsePascal(file, outFile);
+		}
 
-        public static void ParseGroupPrj(string file, string outFile)
-        {
-	        var gp = Groups.ReadProjects(file);
-	        var path = $"{outFile}.txt";
-	        var lines = new[] { gp.Root, string.Empty }.Concat(gp.Links);
-	        File.WriteAllLines(path, lines, Encoding.UTF8);
-        }
+		public static void ParseGroupPrj(string file, string outFile)
+		{
+			var gp = Groups.ReadProjects(file);
+			var path = outFile.Replace(".groupproj", ".sln");
+			var sln = Creator.CreateSolution(gp.Root, gp.Links);
+			Helper.Write(path, sln);
+		}
 
-        public static void ParseDpk(string file, string outFile)
-        {
-	        var gp = Groups.ReadPackage(file);
-	        var path = $"{outFile}.txt";
-	        var lines = new[] { gp.Root, string.Empty }.Concat(gp.Files);
-	        File.WriteAllLines(path, lines, Encoding.UTF8);
-        }
+		public static void ParseDpk(string file, string outFile)
+		{
+			var gp = Groups.ReadPackage(file);
+			var path = $"{outFile}.txt";
+			var lines = new[] { gp.Root, string.Empty }.Concat(gp.Files);
+			File.WriteAllLines(path, lines, Encoding.UTF8);
+		}
 
-        public static void ParseDproj(string file, string outFile)
-        {
-	        var prj = Groups.ReadProject(file);
-	        var path = $"{outFile}.txt";
-	        var lines = new[] { prj.Root, string.Empty }.Concat(prj.Links)
-		        .Concat(new[] { string.Empty }).Concat(prj.Sources).Concat(new[] { string.Empty })
-		        .Concat(prj.Meta.Select(p => $"{p.Key} = {p.Value}"));
-	        File.WriteAllLines(path, lines, Encoding.UTF8);
-        }
-    }
+		public static void ParseDproj(string file, string outFile)
+		{
+			var op = Groups.ReadProject(file);
+			var path = outFile.Replace(".dproj", ".csproj");
+			var name = Path.GetFileNameWithoutExtension(path);
+			var prj = Creator.CreateProject(name, op.Meta, op.Root, op.Sources);
+			Helper.Write(path, prj);
+		}
+
+		static Delphi()
+		{
+			Creator.Relative = Path.GetRelativePath;
+		}
+	}
 }
